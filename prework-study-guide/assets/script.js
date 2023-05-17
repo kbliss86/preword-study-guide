6cffdbf5ecc69d9abd712e8296a0dd2142935039
kbliss86 <kendall.bliss@smsu.edu>
2023-05-17 01:02:11 -0500

    updated project README file

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,6 +1,7 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 for (var x =  0; x < topics.length; x++) {
 console.log(topics[x]);
 }
@@ -16,6 +17,8 @@ if (topic === 'HTML') {
 }
 */
 =======
+=======
+>>>>>>> 1af735c12cda2c5dff79ccb93ee9d290bd166fe4
 var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
 function listTopics() {
@@ -43,5 +46,9 @@ console.log('Here are the topics we learned through prework:');
 listTopics();
 
 console.log('Which topic should we study first?');
+<<<<<<< HEAD
 selectTopic();
 >>>>>>> 0b77ccca36f25e8de174e6ab4751bd3cd829bed6
+=======
+selectTopic();
+>>>>>>> 1af735c12cda2c5dff79ccb93ee9d290bd166fe4
